502d1a45cd19965a909b88bc10b7c12130a6a076
Salvador Reyna <salvadormizarreyna@gmail.com>
2025-08-04 17:17:13 -0500

    mmm

--- app/js/storage.js
+++
@@ -1,7 +0,0 @@
-// storage.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('storageAPI', {
-  leerDatos: () => ipcRenderer.invoke('leer-datos'),
-  guardarDatos: (datos) => ipcRenderer.invoke('guardar-datos', datos),
-});
